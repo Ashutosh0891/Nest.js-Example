3ca7d6e17488bc65db3db516d203db088fdbc847
Ashutosh Adya <64789273+Ashutosh0891@users.noreply.github.com>
2023-05-27 12:42:00 +0530

    Commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+
+@Module({
+    controllers:[AppController],
+    imports: [
+        UserModule,
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: '127.0.0.1',
+          port: 3306,
+          username: 'root',
+          password: 'Ashu@0891',
+          database: 'nestjs',
+          entities: [User],
+          synchronize: true,
+        }),
+        AuthModule,
+        ProfileModule,
+      ],
+})
+
+export class AppModule{}
